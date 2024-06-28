4fe7fa096fd23b507500b9df98bc6f8dbe76df0d
Chris Ohk <utilforever@gmail.com>
2024-06-28 10:38:55 +0900

    feat: Create example "05 - Mutex and Locks"

---
+++ 2_Example/05_Mutex_and_Locks/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+// Shared data
+int counter = 0;
+// Mutex for critical section
+std::mutex mtx;
+
+void increment()
+{
+    for (int i = 0; i < 1000; ++i)
+    {
+        // Lock the mutex
+        mtx.lock();
+
+        // Critical section
+        ++counter;
+
+        // Unlock the mutex
+        mtx.unlock();
+    }
+}
+
+int main()
+{
+    std::vector<std::thread> threads;
+    threads.reserve(1000);
+
+    // Create 10 threads
+    for (int i = 0; i < 1000; ++i)
+    {
+        threads.emplace_back(increment);
+    }
+
+    for (auto& t : threads)
+    {
+        t.join();
+    }
+
+    std::cout << "Final counter value: " << counter << '\n';
+
+    return 0;
+}
